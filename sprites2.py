ef8ba0f2e30b032ff2a975c3c56f7488b1fbafe9
SodaFlame <63527523+SodaFlame@users.noreply.github.com>
2022-01-05 19:57:58 -0300

    Update sprites2.py

--- sprites2.py
+++ sprites2.py
@@ -1,7 +1,6 @@
 from PPlay.window import *
 from PPlay.gameimage import *
 from PPlay.sprite import *
-from config_trabalho import *
 
 jogador = Sprite("r_sheet.png", 44)           #idle, direita, esquerda, costa, frente
 jogador.set_sequence(1, 8,  loop=True)
